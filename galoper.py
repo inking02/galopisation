0df006bd00ade4e9e00aae862c2d5ae8b825bb9a
inking02 <ludomartvicotec@gmail.com>
2024-03-30 13:10:28 -0400

    init variables

--- galoper.py
+++ galoper.py
@@ -1,7 +1,11 @@
 import numpy as np
 from qiskit import QuantumCircuit
 from typing import List, Union
-
+from numpy.typing import NDArray
+from qiskit.quantum_info import SparsePauliOp, Pauli
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import Statevector
+from qiskit.primitives import Estimator
 def exact_evolution(initial_state: QuantumCircuit, hamiltonian: SparsePauliOp, time_values: NDArray[np.float_], observables: List[SparsePauliOp],):
     """
     Simulate the exact evolution of a quantum system in state ‘initial_state‘ under a given
@@ -19,7 +23,11 @@ def exact_evolution(initial_state: QuantumCircuit, hamiltonian: SparsePauliOp, t
     NDArray[np.float_]: The expected values of the observable. Should be of shape
     ‘(len(time_values), len(observables))‘.
     """
+    diag_hamiltonian = None
 
+    evolution_operator = None
+
+    evolution_state = evolution_operator * Statevector(initial_state)
     observables_expected_values = None
 
 
@@ -61,6 +69,7 @@ def trotter_circuit(hamiltonian: SparsePauliOp, total_duration: Union[float, Par
     Returns:
     QuantumCircuit: The circuit of the Trotter evolution operator
     """
+
 
 
     circuit = None
