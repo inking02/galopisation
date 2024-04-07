c4dc18c36205ec89dab5f90d6908ee089d1538f3
WilhemHarvey <156103745+WilhemHarvey@users.noreply.github.com>
2024-04-07 17:24:27 -0400

    Add files via upload
    plus meilleur clean

---
+++ evolution_simulation.py
@@ -0,0 +1,98 @@
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister
+from typing import List, Union
+from numpy.typing import NDArray
+from qiskit.quantum_info import SparsePauliOp, Pauli
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import Statevector
+from qiskit.primitives import Estimator
+from evolution_simulation_utils import trotter_circuit
+
+
+def exact_evolution(
+    initial_state: QuantumCircuit,
+    hamiltonian: SparsePauliOp,
+    time_values: NDArray[np.float_],
+    observables: List[SparsePauliOp],
+):
+    """
+    Simulate the exact evolution of a quantum system in state "initial_state" under a given
+    "hamiltonian" for different "time_values". The result is a series of expected values
+    for given "observables".
+
+    Args:
+    time_values (NDArray[np.float_]): An array of time values.
+    initial_state (QuantumCircuit): The circuit preparing the initial quantum state.
+    hamiltonian (SparsePauliOp): The Hamiltonien of the system
+    observables (List[SparsePauliOp]): The observable to be measure at each the the
+    "time_values".
+
+    Returns:
+    NDArray[np.float_]: The expected values of the observable. Should be of shape
+    "(len(time_values), len(observables))".
+    """
+    hamiltonian_matrix = hamiltonian.to_matrix()
+    eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_matrix)
+
+    w = np.exp(-1j * np.einsum("i, j-> ij", time_values, eigenvalues))
+    evolution_operators = np.einsum(
+        "ik, sk, jk -> sij", eigenvectors, w, np.conjugate(eigenvectors)
+    )
+
+    init_state = Statevector(initial_state)
+    evolved_states = np.einsum("sij, j -> si", evolution_operators, init_state)
+    observables_expected_values = np.einsum(
+        "si, mij, sj -> sm", np.conjugate(evolved_states), observables, evolved_states
+    )
+
+    return observables_expected_values
+
+
+def trotter_evolution(
+    initial_state: QuantumCircuit,
+    hamiltonian: SparsePauliOp,
+    time_values: NDArray[np.float_],
+    observables: List[SparsePauliOp],
+    num_trotter_steps: NDArray[np.int_],
+):
+    """
+    Simulate, using Trotterisation, the evolution of a quantum system in state "initial_state"
+    under a given "hamiltonian" for different "time_values". The result is a series of
+    expected values for given "observables".
+
+    Args:
+    time_values (NDArray[np.float_]): An array of time values.
+    initial_state (QuantumCircuit): The circuit preparing the initial quantum state.
+    hamiltonian (SparsePauliOp): The Hamiltonien of the system
+    observables (List[SparsePauliOp]): The observable to be measure at each the the
+    "time_values".
+    num_trotter_steps: (NDArray[np.int_]): The number of steps of the Trotterization for
+    each "time_values".
+
+    Returns:
+    NDArray[np.float_]: The expected values of the observable. Should be of shape
+    "(len(time_values), len(observables))".
+    """
+    circuits = []
+    observables_list = []
+
+    for time_value, num_trotter_step in zip(time_values, num_trotter_steps):
+        observables_list = observables_list + observables
+        qreg = QuantumRegister(initial_state.num_qubits)
+        circuit = QuantumCircuit(qreg)
+        circuit.append(initial_state.to_gate(label="init_state"), qreg)
+        circuit.append(
+            trotter_circuit(hamiltonian, time_value, num_trotter_step).to_gate(
+                label="trotter_circuit"
+            ),
+            qreg,
+        )
+        for _ in range(len(observables)):
+            circuits.append(circuit)
+
+    estimator = Estimator()
+    results = estimator.run(circuits, observables_list).result().values
+
+    reshape_tuple = (len(time_values), len(observables))
+    observables_expected_values = results.reshape(reshape_tuple)
+    return observables_expected_values
